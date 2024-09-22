9ff1db9b8d5ddd7233a71c1edec24572b9aec528
mickeysh <mickey.shaul@gmail.com>
2024-09-22 07:21:12 -0400

    fix apigw dependency issues

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -51,10 +51,7 @@ resource "aws_api_gateway_integration_response" "response" {
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [
-    aws_api_gateway_integration.lambda_integration,
-    aws_api_gateway_rest_api_policy.bedrock_lambda_api_policy]
-
+  depends_on = [aws_api_gateway_integration.lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.bedrock_lambda.id
 }
 
